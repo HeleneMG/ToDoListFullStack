a128410cbd9a15411e7f5e11ccf4a8d269e2591f
HeleneMG <helenemouttetg@gmail.com>
2019-08-14 09:25:51 +0200

    premier commit

---
+++ projetFullStack.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+
+<head>
+    <meta charset=UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="projetFullStack.css">
+    <title>To do list</title>
+</head>
+
+<body>
+    <h1>Ma To Do List</h1>
+    <div class='content'>
+        <h3 id="todo" class="titre">TO DO</h3>
+        <h3 id="done" class="titre">DONE</h3>
+    </div>
+    <script src="projetFullStack.js"></script>
+</body>
+
+</html>
